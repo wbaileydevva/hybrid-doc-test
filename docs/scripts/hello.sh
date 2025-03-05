9d5c875846bca6f818541b26e725b21331c25dcb
wbaileydevva <wayne.bailey2@va.gov>
2025-03-05 18:17:08 -0500

    updated

--- docs/scripts/hello.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-###########################################################
-#  Script provided as-is without warranty or support.
-#  It is meant to be instructive and may not be functional.
-###########################################################
-
-echo "hello"
